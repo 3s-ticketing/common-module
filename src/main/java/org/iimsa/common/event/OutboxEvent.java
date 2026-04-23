ee7ce5ac4b593cff759d005654a26333203be607
ParkSeYoung128 <tpdudtpdud128@gmail.com>
2026-04-23 15:18:16 +0900

    Init: initial commit

---
+++ src/main/java/org/iimsa/common/event/OutboxEvent.java
@@ -0,0 +1,10 @@
+package org.iimsa.common.event;
+
+public record OutboxEvent(
+        String correlationId,
+        String domainType,
+        String domainId,
+        String eventType,
+        Object payload
+) {
+}
